759316461eebf09614d88f3f77cab99525302c79
zhuichao001 <zhuichao001@163.com>
2021-07-07 17:11:54 +0800

    simplify including files

--- lotus/dialer.h
+++ lotus/dialer.h
@@ -1,20 +1,8 @@
 #ifndef _NET_CARRIAGE_H
 #define _NET_CARRIAGE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/select.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <assert.h>
 #include <map>
-#include <set>
 
 #include "address.h"
 #include "poll.h"
