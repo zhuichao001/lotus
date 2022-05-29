191ff3d233a92481df093f4aba14cef07c6fe38a
zhuichao001 <zhuichao001@163.com>
2022-05-29 17:16:48 +0800

    export http_session_t http_request_t http_response_t

--- lotus/lotus.h
+++ lotus/lotus.h
@@ -9,5 +9,7 @@
 #include "protocol/http.h"
 
 using rpc_session_t = session_t<rpc_request_t, rpc_response_t>;
-
 using rpc_service_t = service_t<rpc_request_t, rpc_response_t>;
+
+using http_session_t = session_t<http_request_t, http_response_t>;
+using http_service_t = service_t<http_request_t, http_response_t>;
